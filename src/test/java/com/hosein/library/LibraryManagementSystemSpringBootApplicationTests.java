76244b9d69dc5b8dc58dcd1b2ea1f5397dba4d80
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-12-19 12:59:20 +0330

    Initial commit

---
+++ src/test/java/com/hosein/library/LibraryManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosein.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
