06f1c1be032ff2c4554f7fd6ec347211e20ec7ff
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-12-19 14:25:10 +0330

    BookController completed

--- src/main/java/com/hosein/library/controller/BookController.java
+++ src/main/java/com/hosein/library/controller/BookController.java
@@ -1,4 +1,40 @@
 package com.hosein.library.controller;
 
+import com.hosein.library.entity.Book;
+import com.hosein.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public Book create(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+
+    @GetMapping
+    public List<Book> getAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
 }
