564c61c480f1a76af1faf6a4bec5c93119d5e32f
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-12-19 14:46:57 +0330

    AuthorController completed

--- src/main/java/com/hosein/library/controller/AuthorController.java
+++ src/main/java/com/hosein/library/controller/AuthorController.java
@@ -1,4 +1,40 @@
 package com.hosein.library.controller;
 
+import com.hosein.library.entity.Author;
+import com.hosein.library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
 public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public Author create(@RequestBody Author author) {
+        return authorService.save(author);
+    }
+
+    @GetMapping
+    public List<Author> getAll() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Author grtById(@PathVariable Long id) {
+        return authorService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        authorService.deleteById(id);
+    }
 }
