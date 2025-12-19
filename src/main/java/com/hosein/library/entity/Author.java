dc00be7e5a6c8d37d72ede11ea096c80766cbe48
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-12-19 13:13:42 +0330

    Author completed

--- src/main/java/com/hosein/library/entity/Author.java
+++ src/main/java/com/hosein/library/entity/Author.java
@@ -1,4 +1,25 @@
 package com.hosein.library.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "authors")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
 }
