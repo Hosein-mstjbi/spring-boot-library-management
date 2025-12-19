16261296738be9ffa482a731e3daf21889546214
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-12-19 14:11:46 +0330

    BookService completed

--- src/main/java/com/hosein/library/service/AuthorService.java
+++ src/main/java/com/hosein/library/service/AuthorService.java
@@ -2,12 +2,10 @@ package com.hosein.library.service;
 
 import com.hosein.library.entity.Author;
 import com.hosein.library.reposiroty.AuthorRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AuthorService {
@@ -28,20 +26,6 @@ public class AuthorService {
 
     public Author findById(Long id) {
         return authorRepository.findById(id).orElseThrow();
-    }
-
-
-    public Author updateById(Long id, Author updateauthor) {
-        Optional<Author> optionalAuthor = authorRepository.findById(id);
-        Author author;
-        if (optionalAuthor.isPresent()) {
-            author = optionalAuthor.get();
-            author.setName(updateauthor.getName());
-            author.setEmail(updateauthor.getEmail());
-            return authorRepository.save(author);
-        } else {
-            throw new EntityNotFoundException("Author not found with id " + id);
-        }
     }
 
     public void deleteById(Long id) {
--- src/main/java/com/hosein/library/service/BookService.java
+++ src/main/java/com/hosein/library/service/BookService.java
@@ -1,4 +1,35 @@
 package com.hosein.library.service;
 
+import com.hosein.library.entity.Book;
+import com.hosein.library.reposiroty.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public Book findById(Long id) {
+        return bookRepository.findById(id).orElseThrow();
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
