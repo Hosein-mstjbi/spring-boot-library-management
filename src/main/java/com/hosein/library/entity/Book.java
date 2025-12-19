7be711cd29bf7b87e3917b810f87131a2b8ea0b8
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2025-12-19 13:19:37 +0330

    Book completed

--- src/main/java/com/hosein/library/entity/Book.java
+++ src/main/java/com/hosein/library/entity/Book.java
@@ -1,4 +1,25 @@
 package com.hosein.library.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisherYear;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
